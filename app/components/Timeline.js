50d4f37b3449c7062e2d532fdfabc760269010d0
Emily Cook <ecook@Launchcg.com>
2017-12-26 16:45:20 -0800

    styles

--- app/components/Timeline.js
+++ app/components/Timeline.js
@@ -48,7 +48,7 @@ class Timeline extends Component {
             <div className='page-header'>
                 <h1 id='timeline'>History of Gen.Chat</h1>
             </div>
-            <span id='SortTimeline' className='sort-icon glyphicon glyphicon-chevron-down'></span>
+            {/*<span id='SortTimeline' className='sort-icon glyphicon glyphicon-chevron-down'></span>*/}
             <ul id='History-timeline' className='col-md-push-2 col-md-8 timeline'>
               {eventNodes}
             </ul>
